ba77fc5a370b73d67ded37e3ba1fdb5033b8fd3c
Khushal Upadhayay <khushalnkpsem@gmail.com>
2025-06-11 00:57:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -160,23 +160,18 @@ class ChatBotAssistant:
             return None
         
 
-def get_stock():
-    stocks = ['APPL', 'META', 'NVDA', 'GS', 'MSFT'] 
-
-    return random.sample(stocks, 3)
-
 if __name__  == '__main__':
 
     ## train the model
-    # assistant = ChatBotAssistant('intents.json', function_mapping= {'stocks': get_stock})
-    # assistant.parse_intents()
-    # assistant.prepare_data()
-    # assistant.train_model(batch_size=8, lr = 0.001, epochs=100)
-    # assistant.save_model('chatbot_model.pth', 'dimensions.json')
-
-    assistant = ChatBotAssistant('intents.json', function_mapping= {'stocks': get_stock})
+    assistant = ChatBotAssistant('intents.json')
     assistant.parse_intents()
-    assistant.load_model('chatbot_model.pth', 'dimensions.json')
+    assistant.prepare_data()
+    assistant.train_model(batch_size=8, lr = 0.001, epochs=100)
+    assistant.save_model('chatbot_model.pth', 'dimensions.json')
+
+    ##assistant = ChatBotAssistant('intents.json', function_mapping= {'stocks': get_stock})
+    ##assistant.parse_intents()
+    ##assistant.load_model('chatbot_model.pth', 'dimensions.json')
 
     while True:
         message = input('Enter your message: ')
